f33361b4ec544be6778d2f943b7af6330e8a6370
Jow <jowarikoda@gmail.com>
2025-03-31 03:28:51 -0300

    feat: Exibe a quantidade de itens no carrinho no Header.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,13 +4,16 @@ import "@/app/components/Header/Header.scss";
 import Image from "next/image";
 import { useState } from "react";
 import CartOverlay from "@/app/components/CartOverlay/CartOverlay";
+import { useAppSelector } from "@/app/redux/store";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const cartItems = useAppSelector((state) => state.cart.items);
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0); // Soma total dos itens do
 
   return (
     <header className="header">
-      <a href="./" className="logo-header">
+      <a href="./">
         <Image
           src="/img/starsoft-logo.svg"
           alt="StarSoft Logo"
@@ -27,6 +30,7 @@ export default function Header() {
           width={33}
           height={33}
         />
+        <span className="cart-count-text">{totalItems}</span>
       </button>
 
       <CartOverlay isOpen={isOpen} setIsOpen={setIsOpen} />
